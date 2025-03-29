19ac3debed6d27fd6b9ca6c1153f76c0f6e9c576
Jeswin A.H <157192053+Jeswinah@users.noreply.github.com>
2025-03-29 11:15:16 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import proimg from "../assets/programerimg.png";
-import Resume from "../assets/resume.pdf";
 import TypingAnimation from "./TypingAnimation";
-
+import Resume from"../../src/Resume.pdf"
 const Hero = () => {
   return (
     <div className="hero flex flex-col lg:flex-row items-center px-5 my-14 lg:py-20 lg:mt-5 min-h-full">
@@ -14,7 +13,7 @@ const Hero = () => {
         />
       </div>
       <div className="flex-1 text-left  lg:pt-20 flex flex-col justify-center items-center lg:block lg:order-1 lg:ml-10 xl:ml-24">
-        <h1 className="text-xl lg:text-2xl xl:text-3xl font-semibold  mb-1 md:mb-2">
+        <h1 className="text-xl lg:text-2xl xl:text-3xl font-semibold mb-1 md:mb-2">
           Hello,It's Me
         </h1>
         <h1 className="text-4xl lg:text-5xl xl:text-7xl font-bold  lg:mb-5 md:mb-4 font-mono ">
@@ -25,33 +24,30 @@ const Hero = () => {
           This is my Portfolio website to showcase all my Web development
           project
         </p>
+
         <div className="btn flex">
-        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-3 px-2  rounded flex mx-2 ">
-          <a
-            href="Resume"
-            download={Resume}
-            name="Resume"
-            className="flex  items-center"
-          >
-            <svg
-              class="fill-current w-4 h-4 mr-2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
+          <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-3 px-2  rounded flex mx-2 ">
+            <a
+              href={Resume}
+              download="Resume.pdf"
+              className="flex  items-center"
             >
-              <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-            </svg>
-            Download CV{" "}
-          </a>
-        </button>
-        <button class="bg-blue-800 hover:bg-gray-400 text-gray-50 font-bold py-3 px-5 rounded flex md:mx-2 ">
-          <a
-            href="#contact"
-            className="flex  items-center"
-          >
-            {" "}
-            Contact us
-          </a>
-        </button>
+              <svg
+                class="fill-current w-4 h-4 mr-2 "
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+              </svg>
+              Download CV{" "}
+            </a>
+          </button>
+          <button class="bg-blue-800 hover:bg-gray-400 text-gray-50 font-bold py-3 px-5 rounded flex md:mx-2 ">
+            <a href="#contact" className="flex  items-center">
+              {" "}
+              Contact us
+            </a>
+          </button>
         </div>
       </div>
     </div>
